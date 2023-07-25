83ae0016d97c08aebf12691fef7f79895a9c32ea
Aaron Randall <aaronasherrandall@gmail.com>
2023-07-25 13:27:10 +0900

    back up copy

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,15 @@ class Restaurant < ApplicationRecord
   # define a constant for the list of valid categories
   VALID_CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
 
+  10.times do
+    restaurant = Restaurant.create!(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: VALID_CATEGORIES.sample # randomly pick one of the categories
+    )
+  end
+
   # add validations here
   validates :name, :address, :category, presence: true
   validates :category, presence: true, inclusion: { in: VALID_CATEGORIES, message: "%{value} us not a valid category" }
